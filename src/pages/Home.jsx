5fbb1ed304b10f03e1a47fecd7987b9ca9b88652
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-11-22 00:07:43 +0700

    add: lower a little ig

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,77 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { Link } from "react-router-dom";
-import { Carousel } from "react-bootstrap";
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules'
 import assets from "../utils/assets";
 import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/scrollbar'
+import 'swiper/css/pagination'
+import '../style/sliderhero.css'
 import '../style/carousel.css'
 import '../style/home.css'
-import SliderHero from "../components/SliderHero";
 
-function CarouselImage({ src }) {
+function SliderHero() {
   return (
-    <div className="hero-container">
-      <div className="overlay"></div>
-      <img className="hero-image" src={src} alt="pic" draggable="false"/>
-    </div>
+    <Swiper
+        slidesPerView={1}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        loop={true}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        className='swiper-hero'
+      >
+      { assets.heroes.map((hero, index) => (
+        <SwiperSlide key={index}> 
+          <div className='overlay'></div>
+          <img src={hero} className="slider-img" alt="hero" draggable="false"/>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
+}
+
+function CarouselText() {
+  return (
+    <section className="carousel-text">
+      <h3 className="font-title">
+        Hamdalla Agil
+      </h3>
+      <h1 className="centered-text font-desc">
+        Illustrator &amp; <br/>
+        Graphic Novel <br/>
+        Artist
+      </h1>
+      <Link to="/portfolio" className="btn-custom-hero">
+        Portfolio
+      </Link>
+      <div className='mt-5'>
+        <ul className='nav justify-content-center mb-3'>
+          { assets.socials.map((social) => (
+            <li className='nav-item footer-item'>
+              <Link to={social.href} target="_blank">
+                <img className="" src={social.logo} alt="" />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   )
 }
 
 function HomeCarousel() {
-
   return (
     <section className="hero-carousel">
       <SliderHero/>
-      <section className="carousel-text">
-        <h3 className="font-title">
-          Hamdalla Agil
-        </h3>
-        <h1 className="centered-text font-desc">
-          Illustrator &amp; <br/>
-          Graphic Novel <br/>
-          Artist
-        </h1>
-        <Link to="/portfolio" className="btn-custom-hero">
-          Portfolio
-        </Link>
-        <div className='mt-5'>
-          <ul className='nav justify-content-center mb-3'>
-            { assets.socials.map((social) => (
-              <li className='nav-item footer-item'>
-                <Link to={social.href} target="_blank">
-                  <img className="" src={social.logo} alt="" />
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
+      <CarouselText/>
     </section>
   )
 }
 
 function HomeAbout() {
-  const [ currWidth, setCurrWidth ] = useState(window.innerWidth)
-  const [ isPhone, setIsPhone ] = useState(currWidth <= 550)
-
+  const isPhone = window.innerWidth <= 550
+  
   const greetDiv = () => {
     return (
       <div className="">
@@ -61,24 +79,6 @@ function HomeAbout() {
       </div>
     )
   }
-
-  const handleResize = () => {
-    setCurrWidth(window.innerWidth)
-    setIsPhone(currWidth <= 550)
-  }
-
-  useEffect(() => {
-    if (window.innerWidth !== currWidth) {
-      handleResize()
-    }
-
-    window.addEventListener('resize', handleResize)
-
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
-
   
   if (isPhone) {
     return (
@@ -98,7 +98,6 @@ function HomeAbout() {
     )
   }
   
-
   return (
     <section className="section-home">
       <section className="home-about-container">
@@ -118,6 +117,14 @@ function HomeAbout() {
   )
 }
 
+function HomeIllustrations() {
+  return (
+    <section>
+      
+    </section>
+  )
+}
+
 export default function HomePage() {
   useEffect(() => {
     document.title = 'Home'
@@ -127,6 +134,7 @@ export default function HomePage() {
     <>
       <HomeCarousel/>
       <HomeAbout/>
+      <HomeIllustrations/>
     </>
   )
 }
--- src/utils/assets.js
+++ src/utils/assets.js
@@ -1,10 +1,16 @@
 import hero1 from '../assets/images/hero1.jpg'
 import hero2 from '../assets/images/hero2.jpg'
 import greet from '../assets/images/greet1.jpg'
+import illust1 from '../assets/images/illust1.jpg'
+import illust2 from '../assets/images/illust2.jpg'
+import illust3 from '../assets/images/illust3.jpg'
+import illust4 from '../assets/images/illust3.jpg'
 import emailLogo from '../assets/icons/social/email.svg'
 import tweetLogo from '../assets/icons/social/twitter.svg'
 import instaLogo from '../assets/icons/social/instagram.svg'
 import webLogo from '../assets/icons/logo.svg'
+
+const _artCategories = ['Illustration', 'Fan Art', 'Game Art']
 
 const assets = {
   heroes: [
@@ -15,8 +21,50 @@ const assets = {
     {href: '', logo:emailLogo },
     {href: 'https://twitter.com/', logo:tweetLogo },
   ],
+  illustraions: [
+    {
+      src: illust1,
+      title: "Starlizght",
+      category: _artCategories[0],
+      paragraf: 
+      `I made this illustration for my favorite person in my life. 
+      This piece have a place in my heart so does she.`
+    },
+    {
+      src: illust2,
+      title: "Ophi's Slash",
+      category: _artCategories[0],
+      paragraf: 
+      `I made this illustration for my personal artwork in my own series called FEAR. 
+      In this piece, I want to show more action of the character and I really love the effect of the action line. 
+      It gives power to the character. `
+    },
+    {
+      src: illust3,
+      title: "Gojohime",
+      category: _artCategories[1],
+      paragraf: 
+      `Gojo and Utahime in Jujutsu Kaisen share excellent interactions in the series, in my opinion.
+      However, despite their compelling dynamic, they receive less screen time. 
+      To celebrate their potential and my love for them, I've decided to create a fan art piece.`
+    },
+    {
+      src: hero1,
+      title: "Fall",
+      category: _artCategories[0],
+      paragraf: `I miss her a lot in fall.`
+    },
+    {
+      src: illust4,
+      title: "Coal & Cogs",
+      category: _artCategories[2],
+      paragraf: 
+      `Coal & Cogs is an indie game project that I made with my colleague in campus. 
+      This game was created for a competition in a game development organization called Raion Community, and we were awarded first place.`
+    }
+  ],
   webLogo,
-  greet
+  greet,
 }
 
 export default assets;
