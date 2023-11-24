78c9dd37c6c44f3bb994c137e40e34872521dec7
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-11-25 00:37:46 +0700

    done: temporary about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,6 @@
 import { useEffect } from "react"
+import pp from '../assets/images/flag_{selflovedude}.jpg'
+import '../style/about.css'
 
 export default function AboutPage() {
   useEffect(() => {
@@ -7,9 +9,24 @@ export default function AboutPage() {
 
   return (
     <>
-      <div>
-        <h1>Hello About</h1>
-      </div>
+      <section className="about-section justify-content-center">
+        <section className="row justify-content-center p-5">
+          <div className="col-md-6 justify-content-center">
+            <section className="about-content container p-5">
+              <h1 className="title-about">About</h1>
+              <p className="desc-about">
+              Hamdalla Agil an illustrator based in indonesia.
+              You can explore more of his artworks through this website and get to know more about him.
+              </p>
+            </section>
+          </div>
+          <div className="col-md-6 justify-content-center">
+            <section className="picture-content">
+              <img src={pp} alt="photo-profile" className="photo-profile img-fluid"/>
+            </section>
+          </div>
+        </section>
+      </section>
     </>
   )
 }
